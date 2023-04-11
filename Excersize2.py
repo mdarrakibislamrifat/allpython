771fe058d1922994b977787572e875b38aaff3ba
rifat <rohanrifat43@gmail.com>
2023-04-11 16:25:46 +0600

    Time

---
+++ Excersize2.py
@@ -0,0 +1,10 @@
+import time
+timestamp=time.strftime('%H:%M:%S')
+name=input("Enter you name:")
+hour=int(input("Enter your hour:"))
+if(hour>5 and hour<12):
+    print("Hello Good morning",name)
+elif(hour>=12 and hour<16):
+    print("Hello good Afternoon",name)
+else:
+    print("Hello good evening",name)
