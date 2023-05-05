3b04d1cd1a017a163b2230bec15bd6b83d941366
rifat <rohanrifat43@gmail.com>
2023-05-05 23:47:53 +0600

    List

---
+++ list.py
@@ -0,0 +1,34 @@
+l=[1,2,3,4,"Rifat",True,6,7,8,9]
+# print(l)
+# print(type(l))
+# print(l[0])
+# print(l[1])
+# print(l[2])
+# print(l[3])
+# print(l[4])
+# print(l[5])
+# print(l[-3])
+# print(len(l)-2)
+# print(l[6-3])
+# print(l[3])
+
+# if 4 in l:
+#     print("yes")
+# else:
+#     print("No")
+# #same things applies for strings as well
+# if "Ri" in l:
+#     print("Yes")
+# else:
+#     print("no")
+
+# print(l)
+# print(l[1:-1])
+# print(l[1:8:2])
+# print(l[:])
+
+lst=[i for i in range(4)]
+print(lst)
+
+lst=[i*i for i in range(4)]
+print(lst)
---
+++ list2.py
@@ -0,0 +1,14 @@
+list=[2,6,3,1,7]
+print(list)
+list.append(8)
+print(list)
+list.sort()
+print(list)
+print(list.index(1))
+print(list.count(2))
+
+m=[900,1000,1100]
+k=list+m
+print(k)
+list.extend(m)
+print(list)
